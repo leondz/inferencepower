fix bug in test set scaling, add _ to prefix

rand_test.py
@@ -66,6 +66,7 @@ def measure_activations(*, scale = 8, outprefix=None):
     :param scale: Perform predictions for 10^scale items
     :param outprefix: Prefix of the file to output to
     """
+    #activations = [torch.nn.ReLU]
     activations = [torch.nn.ReLU, torch.nn.ELU, torch.nn.Hardshrink, torch.nn.Hardtanh, torch.nn.LeakyReLU, torch.nn.LogSigmoid, torch.nn.MultiheadAttention, torch.nn.PReLU, torch.nn.ReLU, torch.nn.ReLU6, torch.nn.RReLU, torch.nn.SELU, torch.nn.CELU, torch.nn.GELU, torch.nn.Sigmoid, torch.nn.Softplus, torch.nn.Softshrink, torch.nn.Softsign, torch.nn.Tanh, torch.nn.Tanhshrink, torch.nn.Threshold, torch.nn.Softmin, torch.nn.Softmax, torch.nn.Softmax2d, torch.nn.LogSoftmax, torch.nn.AdaptiveLogSoftmaxWithLoss, torch.nn.Identity, torch.nn.Linear, torch.nn.Bilinear, torch.nn.Dropout, torch.nn.Dropout2d, torch.nn.Dropout3d, torch.nn.AlphaDropout, torch.nn.BatchNorm1d, torch.nn.BatchNorm2d, torch.nn.BatchNorm3d, torch.nn.GroupNorm, torch.nn.SyncBatchNorm, torch.nn.InstanceNorm1d, torch.nn.InstanceNorm2d, torch.nn.InstanceNorm3d, torch.nn.LayerNorm, torch.nn.LocalResponseNorm]
 
     models = {}
@@ -89,7 +90,7 @@ def measure_activations(*, scale = 8, outprefix=None):
             pass
 
     # run inference
-    pred_items = math.pow(10, scale)
+    pred_items = int(math.pow(10, scale))
     print('building random data')
     test_values = torch.randn(pred_items, D_in)
 
@@ -104,7 +105,7 @@ def measure_activations(*, scale = 8, outprefix=None):
 
 
     experiment = {'train_epochs':train_epochs, 'pred_times':pred_times, 'train_times':train_times, 'pred_items':pred_items, 'cuda_available':torch.cuda.is_available()}
-    outfilename = outprefix + str(time.time()) + '.json'
+    outfilename = outprefix + '_' + str(time.time()) + '.json'
     with open(outfilename, 'w') as outfile:
         outfile.write(json.dumps(experiment))
 