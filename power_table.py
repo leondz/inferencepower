8f06e1e80009f5c3e95a65889f071819d39cb9f8
Leon Derczynski <leonderczynski@gmail.com>
2020-06-11 18:24:07 +0200

    drop missing values; fix axes; add graph labels; add hlines

--- power_table.py
+++ power_table.py
@@ -95,7 +95,16 @@ linecycler = cycle(lines)
 markers = '..vv^^++**'
 markercycler = cycle(markers)
 
+xmin=-0.1
+xmax=8.5
+ymax=0.01
+
 plt.yscale('log')
+plt.hlines([0.005,0.001,0.0005,0.0001,0.00005,0.00001], xmin, xmax, color='0.55', linestyle='dashed', lw=0.4)
+plt.axis(xmin=xmin,xmax=xmax,ymax=ymax)
+plt.ylabel('seconds per instance')
+plt.xlabel('number of instances, 10^n')
+plt.title(d['hardware_name'])
 main_colours = cycle([plt.cm.cool(i) for i in np.linspace(0, 1, len(activation_names))])
 drop_colours =  cycle([plt.cm.autumn(i) for i in np.linspace(0, 1, 6)])
 for i in range(len(activation_names)):
@@ -103,8 +112,10 @@ for i in range(len(activation_names)):
     if '.dropout.' in activation_names[i]:
         plot_colour = next(drop_colours)
     if '.linear.' in activation_names[i]:
-        plot_colour = 'grey'
-    plt.plot(range(9), per_inst_timings[0,i], color=plot_colour,
+        plot_colour = 'black'
+    # scrub zeros
+    values_to_plot = [float('nan') if _v==0 else _v for _v in per_inst_timings[0,i]]
+    plt.plot(range(9), values_to_plot, color=plot_colour,
         linestyle=next(linecycler), lw=1, marker=next(markercycler), ms=6.5,
         markerfacecolor='none')
 plt.legend([_n.split('.')[-1] for _n in activation_names], loc='upper right',
